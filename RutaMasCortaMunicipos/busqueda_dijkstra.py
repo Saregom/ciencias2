369354f88909385cab5e591a63704468bfbd0448
Jhojan98 <jhojanramirez75@gmail.com>
2024-08-30 17:43:19 -0500

    ACtualizacion

--- RutaMasCortaMunicipos/busqueda_dijkstra.py
+++ RutaMasCortaMunicipos/busqueda_dijkstra.py
@@ -1,5 +1,15 @@
 import pandas as pd
-
 csv_df = pd.read_csv('RutaMasCortaMunicipos/Conexiones.csv')
-
 csv_df.head()
+
+from geopy.geocoders import Nominatim
+from geopy.distance import geodesic
+geolocator = Nominatim(user_agent="geoapiExercises")
+coordenadas = {}
+for municipio in csv_df.iloc[:, 0]:
+    location = geolocator.geocode(f"{municipio}, Colombia")
+    if location:
+        coordenadas[municipio] = (location.latitude, location.longitude)
+    else:
+        print(f"No se encontraron coordenadas para {municipio}")
+
