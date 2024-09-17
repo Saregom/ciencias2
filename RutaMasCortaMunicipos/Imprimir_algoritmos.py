1f8cfc07febcf6be4d8164cde79c49fc4028deeb
Saregom <santi26reyes@gmail.com>
2024-09-16 20:20:22 -0500

    implementacion bellman y cambios mapa

--- RutaMasCortaMunicipos/Imprimir_algoritmos.py
+++ RutaMasCortaMunicipos/Imprimir_algoritmos.py
@@ -2,51 +2,69 @@ import json
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+
+from Algoritmos_hn_a import greedy_best_first_search_hn
+from Algoritmos_hn_a import a_star_algorithm
 from Algoritmo_kruskal import kruskal_algoritm
 from Algoritmo_prim import prim_algoritm
+from bellman_ford_algorithm import bellman_ford_algorithm
 
 with open('data/coordenates.json', encoding='utf-8') as f:
     data = json.load(f)
+
 matriz_adyacencia = pd.read_csv('data/distances.csv', index_col=0)
 
-def visualizar(aristas):
-    # aristas = kruskal_algoritm(matriz_adyacencia)
-    # aristas = prim_algoritm(matriz_adyacencia)
+def visualize_map(aristas, choice):
     # Extraer las coordenadas y nombres de los municipios
     municipios = list(data.keys())
-    #print(municipios)
     latitudes = [data[municipio]['latitude'] for municipio in municipios]
     longitudes = [data[municipio]['longitude'] for municipio in municipios]
 
-    # Crear un mapa usando las coordenadas
-    fig = px.scatter_mapbox(
-        lat=latitudes,
-        lon=longitudes,
-        text=municipios,  # Mostrar el nombre del municipio
-        zoom=5,  # Nivel de zoom
-        mapbox_style="open-street-map"  # Estilo del mapa
-    )
+    # Crear mapa
+    fig = go.Figure()
 
     # Añadir las aristas al mapa (conexiones entre municipios)
     for arista in aristas:
-        ciudad_1, ciudad_2, peso= arista
-        
+        ciudad_1, ciudad_2, peso = arista
+
         # Extraer las coordenadas si ambas ciudades existen
         lat_1, lon_1 = data[ciudad_1]['latitude'], data[ciudad_1]['longitude']
         lat_2, lon_2 = data[ciudad_2]['latitude'], data[ciudad_2]['longitude']
         
         # Agregar una línea entre las dos ciudades en el mapa
-        fig.add_trace(go.Scattermapbox(
+        trace = go.Scattermapbox(
             lat=[lat_1, lat_2],
             lon=[lon_1, lon_2],
             mode='lines',
-            line=dict(width=2, color='blue'),  
-            name=f"{ciudad_1} - {ciudad_2}"  
-        ))
+            line=dict(width=2, color='blue')
+        )
+
+        if choice == '3':
+            trace.name = f"{aristas[0][0]} - {ciudad_2}: {peso:.3f}"
+        else:
+            trace.name = f"{ciudad_1} - {ciudad_2}: {peso:.3f}"
+
+        fig.add_trace(trace)
+
+    # Añadir los municipios al mapa como puntos
+    fig.add_trace(go.Scattermapbox(
+        lat=latitudes,
+        lon=longitudes,
+        text=municipios,
+        mode='markers',
+        marker=dict(size=8, color='red'),
+        hoverinfo='lat+lon+text',
+        showlegend=False
+    ))
+
     # Configurar el layout del mapa
     fig.update_layout(
-        title="Mapa de Municipios en Colombia",
-        mapbox=dict(center=dict(lat=4.6, lon=-74.0))  # Centrando el mapa en Colombia
+        title = "Mapa de Municipios en Colombia",
+        mapbox = dict(
+            center=dict(lat=4.6, lon=-74.0),  # Centrando el mapa en Colombia
+            style = "open-street-map",  # Estilo del mapa
+            zoom = 5,  # Nivel de zoom
+        )  
     )
 
     # Mostrar el mapa
@@ -56,16 +74,15 @@ if __name__ == "__main__":
     print("Elige el algoritmo a utilizar:")
     print("1. Kruskal")
     print("2. Prim")
+    print("3. Bellman-Ford")
     
     choice = input("Ingresa el número de la opción deseada: ")
     
     if choice == '1':
-        mst = kruskal_algoritm(matriz_adyacencia)
-        visualizar(mst)
+        visualize_map(kruskal_algoritm(matriz_adyacencia), choice)
     elif choice == '2':
-        mst = prim_algoritm(matriz_adyacencia)
-        visualizar(mst)
+        visualize_map(prim_algoritm(matriz_adyacencia), choice)
+    elif choice == '3':
+        visualize_map(bellman_ford_algorithm(), choice)
     else:
         print("Opción no válida.")
-        mst = []
-    
--- RutaMasCortaMunicipos/bellman_ford_algorithm.py
+++ RutaMasCortaMunicipos/bellman_ford_algorithm.py
@@ -1,13 +1,16 @@
 import pandas as pd
 
-# Algoritmo de Bellman-Ford
+df = pd.read_csv('data/distances.csv', index_col=0)
+cities = df.columns.tolist()
+distance_matrix = df.to_numpy() # Matriz de distancias sin nombres de ciudades
+
 def bellman_ford(distance_matrix, origin_city):
     num_cities = len(distance_matrix)
     distances = [float('inf')] * num_cities
     predecessors = [-1] * num_cities
     distances[origin_city] = 0
 
-    # Relajación de las aristas
+    # Relajacion de las aristas
     for i in range(num_cities - 1):
         for j in range(num_cities):
             for k in range(num_cities):
@@ -16,7 +19,7 @@ def bellman_ford(distance_matrix, origin_city):
                         distances[k] = distances[j] + distance_matrix[j][k]
                         predecessors[k] = j
 
-    # Detección de ciclos de peso negativo. Como no hay distancias negativas, no es necesario
+    # Deteccion de ciclos de peso negativo. Como no hay distancias negativas, no es necesario
     # for j in range(num_cities):
     #     for k in range(num_cities):
     #         if distance_matrix[j][k] != 0 and distances[j] + distance_matrix[j][k] < distances[k]:
@@ -37,17 +40,32 @@ def reconstruct_path(predecessors, origin_city, destination_city):
         return path
     else:
         return []  # No hay camino
+    
+# Devuelve el formato (ciudad1, ciudad2, peso) para mostrar en mapa
+def bellman_ford_algorithm():
+    origin_city = 'Leticia'
+    origin_city_index = cities.index(origin_city)
+    distances, predecessors = bellman_ford(distance_matrix, origin_city_index)
+    conections_list = []
 
+    # Mostrar distancias y caminos
+    for i, distance in enumerate(distances):
+        path = reconstruct_path(predecessors, origin_city_index, i)
+        path_cities = [cities[c] for c in path]
+        if len(path_cities) > 1:
+            conections_list.append((path_cities[-2], path_cities[-1], distance))
+        else:
+            # conections_list.append((path_cities[-1], path_cities[-1], distance))
+            conections_list.insert(0, (path_cities[-1], path_cities[-1], distance))
+    
+    return conections_list
+    
+# Si se ejecuta el script de forma independiente para mostrar los caminos para cada ciudad
 if __name__ == "__main__":
-    # Cargar la matriz de distancias desde un archivo CSV
-    df = pd.read_csv('data/distances.csv', index_col=0)
-    cities = df.columns.tolist()
-    distance_matrix = df.to_numpy()
-
     origin_city = 'Bogotá'
     origin_city_index = cities.index(origin_city)
 
-    distances, predecessors = bellman_ford(distance_matrix, origin_city_index)
+    distances, predecessors = bellman_ford(distance_matrix, cities.index(origin_city))
 
     # Mostrar distancias y caminos
     print(f"\nDistancias desde {origin_city}:")
@@ -57,4 +75,5 @@ if __name__ == "__main__":
         else:
             path = reconstruct_path(predecessors, origin_city_index, i)
             path_cities = [cities[c] for c in path]
+
             print(f"{cities[i]}: {distance} {path_cities}")
